38631daba926653102c53b695bb5304a8420ee87
Manar Aldossari <41876645+manarsaaldossari@users.noreply.github.com>
2021-03-28 09:01:02 +0300

    create end page PHP
    - Show current and max score.
    - Access the database and update the max score if necessary.

---
+++ Quiz-App-Master/end-Page.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Congrats!</title>
+    <link rel="stylesheet" href="general.CSS">
+</head>
+
+<body>
+<div class='content-div'>
+        <div id='end' class='flex-center flex-column'>
+
+            <h1> Quiz Time</h1>
+    <?php
+        $username = $_COOKIE["username"];
+        $currentScore = isset($_COOKIE["currentScore"])?$_COOKIE["currentScore"]:0;
+        $databaseName = "Quiz-App";
+        $database = mysqli_connect( "localhost","root", "", $databaseName);
+        if(!$database){
+            die("database connection faild: ".mysqli_connect_error());
+        }
+        //Querys
+        $maxScore_Query  = "SELECT `maxScore` FROM user WHERE `username` =\"$username\" LIMIT 1";
+
+        //check username
+        $maxScoreResult = mysqli_query($database,$maxScore_Query);
+        $row = $maxScoreResult->fetch_assoc();
+        $maxScore = $row["maxScore"];
+        //username match
+        
+        print("<span class='textbox'>Current Score ".$currentScore." </span>");
+        print("<span class='textbox'>");
+        if($currentScore>$maxScore){
+            print("New ");
+            $newmaxScore_Query  = "UPDATE `user` SET `maxScore`= $currentScore WHERE `username` =\"$username\" LIMIT 1";
+            mysqli_query($database,$newmaxScore_Query);
+            $maxScore = $currentScore;
+        }
+        //Close MySQL Connection
+        mysqli_close($database);
+        print("Max Score ".$maxScore." </span> </span>");
+    ?>
+            <a class='button' href='quiz-Page.html'>Play Again</a>
+            <a class='button' href='index.html'>Go Home</a>
+        </div>
+    </div>
+</body>
+
+</html>
